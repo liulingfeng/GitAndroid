c464fbf8b180471bb51df8d98391ade92e1259bd
liulf <854034241@qq.com>
2017-09-25 13:57:28 +0800

    添加自定义Application

---
+++ app/src/main/java/llf/gitandroid/App.java
@@ -0,0 +1,15 @@
+package llf.gitandroid;
+
+import android.app.Application;
+
+/**
+ * Created by llf on 2017/9/25.
+ * 自定义Application
+ */
+
+public class App extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
