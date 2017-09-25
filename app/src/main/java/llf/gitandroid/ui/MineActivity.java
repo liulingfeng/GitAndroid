a73bafb3813977faf84209006f1ba5a8ade60c90
liulf <854034241@qq.com>
2017-09-25 14:14:27 +0800

    添加我的模块

---
+++ app/src/main/java/llf/gitandroid/ui/MineActivity.java
@@ -0,0 +1,20 @@
+package llf.gitandroid.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import llf.gitandroid.R;
+import llf.gitandroid.base.BaseActivity;
+
+/**
+ * Created by llf on 2017/9/25.
+ * 我的模块
+ */
+
+public class MineActivity extends BaseActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mine);
+    }
+}
