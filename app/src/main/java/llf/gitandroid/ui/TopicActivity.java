2fe960a0648fc957570f9624399a810edb174ded
liulf <854034241@qq.com>
2017-09-25 14:17:00 +0800

    添加社区模块

---
+++ app/src/main/java/llf/gitandroid/ui/TopicActivity.java
@@ -0,0 +1,20 @@
+package llf.gitandroid.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import llf.gitandroid.R;
+import llf.gitandroid.base.BaseActivity;
+
+/**
+ * Created by llf on 2017/9/25.
+ * 社区模块
+ */
+
+public class TopicActivity extends BaseActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_topic);
+    }
+}
